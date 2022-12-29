3747b5fae8b4a98e6e3acfa1190e92e146f8250f
Илья <storojew.i123@gmail.com>
2022-12-29 15:43:22 +0300

    Выполнил задачу 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int age = 18;
+        System.out.println("Если возраст человека равен " + age + ", то");
+        if (age >= 18) {
+            System.out.println("он совершеннолетний");
+        } else {
+            System.out.println("он не достиг совершеннолетия, нужно немного подождать");
+        }
+
     }
 }
